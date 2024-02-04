adf00c76d7fd99ee077e79f2cff8096e4e53c568
AMD794 <2952277346@qq.com>
2024-02-04 18:46:31 +0800

    update article

---
+++ scripts/head_end.js
@@ -0,0 +1,12 @@
+hexo.extend.injector.register('head_end', `
+    <script id="LA_COLLECT" charset="UTF-8" src="//sdk.51.la/js-sdk-pro.min.js"></script>
+    <script>LA.init({id: "3Gbf5SJIqASrRpwv", ck: "3Gbf5SJIqASrRpwv"})</script>
+    
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "kwgf4jvqf3");
+    </script>
+`);
